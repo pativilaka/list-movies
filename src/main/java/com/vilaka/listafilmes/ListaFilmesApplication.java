6c98cf6d2388ccbbc0ce1f621b5df1be8960056f
pativilaka <p.vilaka@gmail.com>
2023-12-27 16:39:54 -0300

    first commit

---
+++ src/main/java/com/vilaka/listafilmes/ListaFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.vilaka.listafilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaFilmesApplication.class, args);
+	}
+
+}
